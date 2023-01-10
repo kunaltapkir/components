6c3c8d751591af3003f6a4706e5eec0d120e7951
kunaltapkir <kunaltapkir75@gmail.com>
2023-01-10 14:20:04 +0530

    button, accordion components added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const items = [
+    {
+      id: "123a",
+      label: "Can I use React on a project?",
+      content: "You can use React on any project you want",
+    },
+    {
+      id: "456b",
+      label: "Can I use Javascript on a project?",
+      content: "You can use Javascript on any project you want",
+    },
+    {
+      id: "789c",
+      label: "Can I use CSS on a project?",
+      content: "You can use CSS on any project you want",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,44 @@
+import Button from "../components/Button";
+import { GoCheck, GoCreditCard, GoEye, GoAlert, GoFlame } from "react-icons/go";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("Click!!");
+  };
+  return (
+    <div>
+      <div>
+        <Button primary rounded onClick={handleClick} className="mb-5">
+          <GoCheck />
+          Click Me!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline onMouseEnter={handleClick}>
+          <GoCreditCard />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <GoEye />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          <GoAlert />
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded>
+          <GoFlame />
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
