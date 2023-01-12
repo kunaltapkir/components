b895d64bfdcab765fe0c806fa94fc383427fbfed
kunaltapkir <kunaltapkir75@gmail.com>
2023-01-12 12:19:23 +0530

    sorting added.

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -28,6 +29,7 @@ function TablePage() {
     {
       label: "Name",
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     {
       label: "Color",
@@ -36,6 +38,7 @@ function TablePage() {
     {
       label: "Score",
       render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
@@ -45,7 +48,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
